7daf84f44e25cba8fef3289098a26681de53ea10
Viraj Dhumal <mevirajdhumal@gmail.com>
2023-04-09 23:52:21 +0530

    Completed basic Musem app

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, ExploreContainerComponent]
+})
+export class Tab4Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
